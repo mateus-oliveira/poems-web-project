30c6f40f1b12dbf42359475f4a4e95ad4149a5da
Mateus Oliveira <devmateusalves@gmail.com>
2024-08-18 18:04:05 -0300

    feat: DAOs modules

---
+++ api/app/daos/poems_dao.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import Session
+from app import models
+
+
+def create_poem(db: Session, poem: models.PoemCreate, owner_id: int):
+    db_poem = models.Poem(title=poem.title, content=poem.content, owner_id=owner_id)
+    db.add(db_poem)
+    db.commit()
+    db.refresh(db_poem)
+    return db_poem
+
+
+def get_poems(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Poem).offset(skip).limit(limit).all()
+
+
+def get_poem_by_id(db: Session, poem_id: int):
+    return db.query(models.Poem).filter(models.Poem.id == poem_id).first()
+
+
+def update_poem(db: Session, poem_id: int, title: str, content: str):
+    db_poem = get_poem_by_id(db, poem_id)
+    if db_poem:
+        db_poem.title = title
+        db_poem.content = content
+        db.commit()
+        db.refresh(db_poem)
+    return db_poem
---
+++ api/app/daos/users_dao.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from app import models
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def create_user(db: Session, user: models.UserCreate, hashed_password: str):
+    db_user = models.User(email=user.email, name=user.name, password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
